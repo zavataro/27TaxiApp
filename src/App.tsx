1e96730fd24bb053881924cea2da8e6cf44f2d15
zavataro <zavataro@gmail.com>
2025-02-10 17:46:10 -0300

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { user, signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -16,23 +18,16 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>Seja Bem vindo {user?.signInDetails?.loginId}</h1>
+      <h1>ERRO AO CARREGAR INFORMAÇÕES</h1>
+      <button onClick={signOut}>Sair</button>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { I18n } from 'aws-amplify/utils';
+import { Authenticator, translations, View, Image, useTheme } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
+import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+I18n.putVocabularies(translations);
+I18n.setLanguage('pt-br');
+
+
+I18n.putVocabularies({
+ "pt-br" : {
+    'Sign In': 'Entrar',
+    'Enter your Email' : 'Digite seu Email',
+    'Enter your email' : 'Digite seu Email',
+    'Sign Up': "Sair",
+    'Create Account' : 'Seja um Associado',
+    'Password' : 'Senha',
+    'Enter your Password' : 'Digite sua Senha',
+  'Reset Password' : 'Recupere sua Senha',
+  'Email' : 'Email',
+  },
+});
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="27 Taxi"
+          src="./src/assets/logo.jpg"
+          width="100px"
+        />
+      </View>
+    );
+  },
+}
+
 Amplify.configure(outputs);
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator components={components}>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
